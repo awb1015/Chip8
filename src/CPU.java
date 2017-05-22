782c7519b12bfab72a1e495c675aca3e753efc71
Alex <alexander.w.baer@gmail.com>
2017-05-22 23:35:50 +0100

    More Opcodes (again)
    Went through the entire 0x8--- series of opcodes and they're all
    completed now aside from a carry flag to be added for 8XY4. Also added a
    few others, 9XY0 and CXNN.

--- src/CPU.java
+++ src/CPU.java
@@ -2,7 +2,7 @@
 import java.util.Stack;
 
 public class CPU {
-	
+
 	//fields
 	private char[] memory;
 	private char[] V;
@@ -18,7 +18,7 @@ public class CPU {
 	private char delay_timer;
 	private char sound_timer;
 	private boolean drawFlag;
-	
+
 	//Constructor
 	public CPU(){
 		//Initialize registers and memory
@@ -34,28 +34,28 @@ public class CPU {
 			memory[i]= FontSet.getFontSetEntry(i);
 		}
 	}
-	
+
 	//Emulate cycle
 	public void emulateCycle(){
 		//Fetch Opcode (not sure this is OK with type cast)
 		opcode = (short) (memory[pc] << 8 | memory[pc+1]);
-		
+
 		//Decode Opcode & Execute Opcode
 		//At the moment this is set-up as a case structure perhaps a hashtable is better?
 		//Just check the first four bits for our cases
 		switch(opcode & 0xF000){
-		
+
 		case 0x1000: //1NNN This Jumps to the address at NNN
 			//Unlike 0x2NNN there's no need to store the program count to the stack
 			pc = (short)(opcode & 0x0FFF);
-		break;
-		
+			break;
+
 		case 0x2000: //2NNN This calls the subroutine at NNN
 			//Store our current state in the stack
 			stack.push(pc);
 			pc = (short) (opcode & 0x0FFF);
-		break;
-		
+			break;
+
 		case 0x3000: //3XNN This skips the next code block if V[X] equals NN
 			if((opcode & 0x00FF) == V[(opcode & 0x0F00)>>8]){
 				pc += 4;
@@ -63,8 +63,8 @@ public class CPU {
 			else{
 				pc +=2;
 			}
-		break;
-		
+			break;
+
 		case 0x4000: //4XNN This skips the next instruction if V[x]!=NN
 			if((opcode & 0x00FF) != V[(opcode & 0x0F00)>>8]){
 				pc += 4;
@@ -72,8 +72,8 @@ public class CPU {
 			else{
 				pc += 2;
 			}
-		break;
-		
+			break;
+
 		case 0x5000: //5XY0 Skips the next instruction if Vx = Vy
 			if(V[((opcode & 0x0F00)>>8)] == V[((opcode & 0x00F0) >> 4)]){
 				pc+=4;
@@ -81,63 +81,85 @@ public class CPU {
 			else{
 				pc+=2;
 			}
-		break;
-		
+			break;
+
 		case 0x6000: //6XNN Sets Vx to NN
 			V[((opcode & 0x0F00) >> 8)] =(char) (opcode & 0x00FF);
 			pc+=2;
-		break;
-		
+			break;
+
 		case 0x7000: //7XNN Adds NN to Vx
 			V[((opcode & 0x0F00)>>8)] = (char)((V[((opcode & 0x0F00)>>8)]) + (char)(opcode & 0x00FF));		
-		break;
-		
-		//Now a fair amount of cases for our 8XXX series
+			break;
+
+			//Now a fair amount of cases for our 8XXX series
 		case 0x8000:
 			//Now compare last four bits
 			switch(opcode & 0x000F)
 			{
-				case 0x0000: //8XY0 Sets Vx to value of Vy
-					//Assignment here
+			case 0x0000: //8XY0 Sets Vx to value of Vy
+				V[((opcode & 0x0F00)>>8)] = V[((opcode & 0x00F0)>>4)];
+				pc +=2;
 				break;
-				
-				case 0x0001: // 8XY1 Sets Vx to Vx or Vy
-					//Bit-wise op here
+
+			case 0x0001: // 8XY1 Sets Vx to Vx or Vy
+				V[((opcode & 0x0F00)>>8)] = (char) ((V[((opcode & 0x0F00)>>8)])|(V[((opcode & 0x00F0)>>4)]));
+				pc +=2;
 				break;
-				
-				case 0x0002: //8XY2 Sets Vx to Vx and Vy
-					//Bit-wise op here
+
+			case 0x0002: //8XY2 Sets Vx to Vx and Vy
+				V[((opcode & 0x0F00)>>8)] = (char) ((V[((opcode & 0x0F00)>>8)]) & (V[((opcode & 0x00F0)>>4)]));
+				pc +=2;
 				break;
-				
-				case 0x0003: //8XY3 Sets Vx to Vx XOR Vy
-					//Bit-wise op here
+
+			case 0x0003: //8XY3 Sets Vx to Vx XOR Vy
+				V[((opcode & 0x0F00)>>8)] = (char) ((V[((opcode & 0x0F00)>>8)]) ^ (V[((opcode & 0x00F0)>>4)]));
+				pc +=2;
 				break;
-				
-				case 0x0004: //8XY4 Adds Vx to Vy if there is a carry set VF=1
-					//Addition and if statement in here
+
+			case 0x0004: //8XY4 Adds Vx to Vy if there is a carry set VF=1
+				if(V[((opcode & 0x00F0)>>4)]>(0xFF - V[((opcode & 0x0F00)>>8)])){
+					//Then we need to carry
+					V[0xF] = 1;
+				}
+				else{
+					V[0xF] = 0;
+				}
+				V[((opcode & 0x0F00)>>8)] = (char) (V[((opcode & 0x0F00)>>8)] + V[((opcode & 0x00F0)>>4)]);
+				pc +=2;
 				break;
-				
-				case 0x0005: //8XY5 Vy is subtracted from Vx. Set VF=1 when there is a borrow
-					//Substraction and if statement in here
+
+			case 0x0005: //8XY5 Vy is subtracted from Vx. Set VF=1 when there is a borrow
+				//Some condition needed here to check for the borrow
+				if(){
+					V[0xF] = 1;
+				}
+				else{
+					V[0xF] = 0;
+				}
+				V[((opcode & 0x0F00)>>8)] = (char) (V[((opcode & 0x0F00)>>8)] - V[((opcode & 0x00F0)>>4)]);
+				pc +=2;
 				break;
-				
-				case 0x0006: //8XY6 bit shift Vx by on. Vf is set to least significant bit of Vx before shift
-					//bit shift here
+
+			case 0x0006: //8XY6 bit shift Vx by on. Vf is set to least significant bit of Vx before shift
+				V[0xF] = (char) ((V[((opcode & 0x0F00)>>8)]) & 0x1);
+				V[((opcode & 0x0F00)>>8)] = (char) ((V[((opcode & 0x0F00)>>8)])>>1);
 				break;
-				
-				case 0x0007: //8XY7 Vx = Vy - Vx Set Vf to 0 when there is a borrow
-					//Substraction and if statement in here
+
+			case 0x0007: //8XY7 Vx = Vy - Vx Set Vf to 0 when there is a borrow
+				//Substraction and if statement in here
 				break;
-				
-				case 0x000E: //8XYE Shifts Vx left by one. Vf is set to most significant bit prior to shift
-					//Bitwise and VF set here
+
+			case 0x000E: //8XYE Shifts Vx left by one. Vf is set to most significant bit prior to shift
+				V[0xF] = (char) (((V[((opcode & 0x0F00)>>8)]) & 0x80)>>8);
+				V[((opcode & 0x0F00)>>8)] = (char) ((V[((opcode & 0x0F00)>>8)])<<1);
 				break;
-					
-				default:
-					System.out.println("Unknown opcode at 0x8000" + opcode);
-			
+
+			default:
+				System.out.println("Unknown opcode at 0x8000" + opcode);
+
 			}
-		
+
 		case 0x9000: //9XY0 Skips the next instruction if Vx!=Vy
 			if(V[((opcode & 0x0F00)>>8)] != V[((opcode & 0x00F0) >> 4)]){
 				pc+=4;
@@ -145,50 +167,92 @@ public class CPU {
 			else{
 				pc+=2;
 			}
-		break;
-		
+			break;
+
 		case 0xA000: //ANNN Sets I to the address NNN
 			I = (short) (opcode & 0x0FFF);
 			//Increase program counter to next pair of 2 bits
 			pc += 2;
-		break;
-		
+			break;
+
 		case 0xB000: //BNNN Jumps to address NNN +V0
 			//This is WRONG
-			pc = (opcode & 0x0FFF) + V[0];			
-		break;
+			pc = (short) ((opcode & 0x0FFF) + V[0]);			
+			break;
+
+		case 0xC000: //CXNN Sets Vx to be result of bitwise OR between NN and Random Num
+			V[((opcode & 0x0F00)>>8)] = (char) (((opcode & 0x00FF) | (0+ (int)(Math.random() * ((255-0) + 1))))); 
+			pc+= 2;
+			break;
+		
+		case 0xD000: //DXYN Draws a sprite at VX, VY that is 8 pixels wide and N pixels high
+			//More to come
+			break;
+			
+		case 0xE000: //Two 0xE000 opcodes here, time for a deeper level of case
+			switch(opcode & 0x000F){
+			
+			case 0x000E: //EX9E Skips the next instruction if the key at Vx is pressed
+				if(){
+					pc+=4;
+				}
+				else{
+					pc+=2;
+				}
+				
+				break;
+				
+			case 0x0001: //EXA1 Skips the next instruction if the key stored at Vx isn't pressed
+				if(){
+					pc+=4;
+				}
+				else{
+					pc+=2;
+				}
+				
+				break;
+			
+			default:
+				System.out.println("Unknown Opcode [0xE000] : 0x" + opcode);
+			
+			}
+			
+		case 0xF000:
+			//Loads in here we'll need another case statement
+			break;
 		
 		case 0x0000:
 			//Now we need to compare the last four bits
 			switch (opcode & 0x000F)
 			{
-				//This case below has a last 4 bits of 0 and first four of zero
-				case 0x0000: // 0x00E0: Clear Screen
-					//Something here to clear the screen
-					drawFlag = true;
+			//This case below has a last 4 bits of 0 and first four of zero
+			case 0x0000: // 0x00E0: Clear Screen
+				//Something here to clear the screen
+				drawFlag = true;
 				break;
-				
-				case 0x000E: // 0x00EE : Returns from subroutine
-					//We need to restore address value from the stack
-					//Presumably no need to increase pc by two as this has been done before it was pushed to the stack
-					pc = stack.pop();
+
+			case 0x000E: // 0x00EE : Returns from subroutine
+				//We need to restore address value from the stack
+				pc = stack.pop();
+				pc+=2;
 				break;
-				
-				default:
-					System.out.println("Unknown Opcode [0x0000]: 0x" + opcode);
+
+			default:
+				System.out.println("Unknown Opcode [0x0000]: 0x" + opcode);
+			
 			}
-		
-						
+			
+			
 		default:
 			System.out.println("Unknown OpCode at 0x" + opcode);
-		
+
 		}
-		
+
 		//Update Timers
 		if(delay_timer > 0){
 			--delay_timer;
 		}
-		
+
 		if(sound_timer > 0){
 			if(sound_timer == 1){
 				//Or some real sound one day
@@ -196,18 +260,17 @@ public class CPU {
 			}
 			--sound_timer;
 		}
-		
+
 	}
-	
+
 	public boolean getDrawFlag(){
-		
 		//Simple return statement to allow for things to play nicely
 		return drawFlag;
 	}
-	
+
 	//Set our keys here
 	public void setKeys(){
-	
+
 	}
-	
+
 }
